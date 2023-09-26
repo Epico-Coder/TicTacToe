92493629ab9ee826bc875523ba1064c9d46a47fe
Epico-Coder <aaryanjha31@gmail.com>
2023-09-27 00:25:55 +0400

    ui changes, comments

--- game.py
+++ game.py
@@ -1,51 +1,43 @@
 import pygame
 
-WIDTH, HEIGHT = 1280, 720
-BOARD_SIZE = 0.5
-BG = pygame.image.load("bg.webp")
-BG = pygame.transform.scale(BG, (WIDTH, HEIGHT))
-win = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
 
+def draw_x(i, j, board_size, width, height, win):
+    board_x = width // 2 - (board_size * width) // 2
+    board_y = height // 2 - (board_size * height) // 2
 
-def draw_x(i, j):
-    board_x = WIDTH // 2 - (BOARD_SIZE * WIDTH) // 2
-    board_y = HEIGHT // 2 - (BOARD_SIZE * HEIGHT) // 2
-
-    cell_width = BOARD_SIZE * WIDTH // 3
-    cell_height = BOARD_SIZE * HEIGHT // 3
+    cell_width = board_size * width // 3
+    cell_height = board_size * height // 3
 
     size = int(0.8 * min(cell_width, cell_height))
 
     x = (board_x + (j * cell_width) + (cell_width // 2)) - (size / 2)
     y = (board_y + (i * cell_height) + (cell_height // 2)) - (size / 2)
 
-    line_color = (255, 255, 255)  # White
-    border_color = (0, 0, 0)  # Black
+    line_color = (255, 255, 255)
+    border_color = (0, 0, 0)
     border_thickness = 2
 
-    # Top-left to Bottom-right
     pygame.draw.line(win, border_color, (x, y), (x + size, y + size), size // 3)
     pygame.draw.line(win, line_color, (x, y), (x + size, y + size), size // 3 - border_thickness)
 
-    # Bottom-left to Top-right
     pygame.draw.line(win, border_color, (x, y + size), (x + size, y), size // 3)
     pygame.draw.line(win, line_color, (x, y + size), (x + size, y), size // 3 - border_thickness)
 
 
-def draw_o(i, j):
-    board_x = WIDTH // 2 - (BOARD_SIZE * WIDTH) // 2
-    board_y = HEIGHT // 2 - (BOARD_SIZE * HEIGHT) // 2
+def draw_o(i, j, board_size, width, height, win):
+    board_x = width // 2 - (board_size * width) // 2
+    board_y = height // 2 - (board_size * height) // 2
 
-    cell_width = BOARD_SIZE * WIDTH // 3
-    cell_height = BOARD_SIZE * HEIGHT // 3
+    cell_width = board_size * width // 3
+    cell_height = board_size * height // 3
 
     size = int(0.8 * min(cell_width, cell_height))
 
     x = (board_x + (j * cell_width) + (cell_width // 2)) - (size / 2)
     y = (board_y + (i * cell_height) + (cell_height // 2)) - (size / 2)
 
-    border_color = (0, 0, 0)  # Black
-    circle_color = (255, 255, 255)  # White
+    border_color = (0, 0, 0)
+    circle_color = (255, 255, 255)
     border_thickness = 2
 
     pygame.draw.circle(win, border_color, (x + size // 2, y + size // 2), size // 2, size // 8)
@@ -53,7 +45,7 @@ def draw_o(i, j):
                        size // 8 - border_thickness)
 
 
-def draw_board(x, y, width, height):
+def draw_board(x, y, width, height, win):
     line_color = (255, 255, 255)
     border_color = (255, 0, 0)
     border_thickness = 5
@@ -70,7 +62,7 @@ def draw_board(x, y, width, height):
     pygame.draw.rect(win, border_color, (x, y, width, height), border_thickness)
 
 
-def draw_bar(x, y, width, height, value):
+def draw_bar(x, y, width, height, value, win):
     white_color = (255, 255, 255)
     black_color = (0, 0, 0)
     separator_color = (150, 150, 150)
--- main.py
+++ main.py
@@ -2,61 +2,80 @@ import pygame
 import time
 
 from ai import minimax, get_best_move, load_cache, save_cache
-from game import draw_x, draw_o, draw_board, draw_bar, win, WIDTH, HEIGHT, BOARD_SIZE, BG
+from game import draw_x, draw_o, draw_board, draw_bar
 from utils import Position
 
 pygame.init()
 
-new = Position()
+# constants
+WIDTH, HEIGHT = 1280, 720
 
+BG = pygame.image.load("bg.webp")
+BG = pygame.transform.scale(BG, (WIDTH, HEIGHT))
+
+BOARD_SIZE = 0.75
+
+win = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
 pygame.display.set_caption("TicTacToe")
+
+new = Position()
 
 cache = load_cache("cache.pkl")
 
-
+# re draw ui every frame
 def re_draw_win():
     win.blit(BG, (0, 0))
 
     draw_board(WIDTH // 2 - (BOARD_SIZE * WIDTH) // 2,
                HEIGHT // 2 - (BOARD_SIZE * HEIGHT) // 2,
                BOARD_SIZE * WIDTH,
-               BOARD_SIZE * HEIGHT)
+               BOARD_SIZE * HEIGHT,
+               win)
 
     evaluation = minimax(position=new, depth=0, maximizing_player=new.turn, cache=cache)
     normalized = (evaluation + 10) / 20
-    draw_bar(0, 0, WIDTH, 0.1 * HEIGHT, normalized)
+    draw_bar(0, 0, WIDTH, 0.1 * HEIGHT, normalized, win)
 
     for i, row in enumerate(new.position):
         for j, ele in enumerate(row):
             if ele == 1:
-                draw_x(i, j)
+                draw_x(i, j, BOARD_SIZE, WIDTH, HEIGHT, win)
             elif ele == -1:
-                draw_o(i, j)
+                draw_o(i, j, BOARD_SIZE, WIDTH, HEIGHT, win)
 
     pygame.display.update()
 
 
 def main():
     global BG, WIDTH, HEIGHT
+    run = True
 
-    while True:
+    clock = pygame.time.Clock()
+    clock.tick(30)
+
+    while run:
+
+        # ai's turn if game not over
         if type(new.result) != int:
             if new.turn == -1:
                 move = get_best_move(new, new.turn, cache)
                 new.make_move(move)
 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
 
+            # exit
+            if event.type == pygame.QUIT:
+                run = False
+
+            # resize ui
             if event.type == pygame.WINDOWRESIZED:
-                BG = pygame.transform.scale(BG, (WIDTH, HEIGHT))
                 WIDTH = win.get_width()
                 HEIGHT = win.get_height()
+                BG = pygame.transform.scale(BG, (WIDTH, HEIGHT))
 
-            if type(new.result) != int:
-                if event.type == pygame.MOUSEBUTTONDOWN:
+            # find row and col, make move
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if type(new.result) != int:
                     mouse_x = event.pos[0]
                     mouse_y = event.pos[1]
 
@@ -70,23 +89,24 @@ def main():
                     clicked_col = int((mouse_x - board_x) // cell_width)
 
                     new.make_move((clicked_row, clicked_col))
-            else:
-                if new.result == 1:
-                    print("X wins!")
-                elif new.result == 0:
-                    print("Draw!")
-                elif new.result == -1:
-                    print("O wins!")
-
-                time.sleep(3)
-                return
 
         re_draw_win()
 
+    # after quit
+    else:
+        pygame.quit()
+
+        if new.result == 1:
+            print("X wins!")
+        elif new.result == 0:
+            print("Draw!")
+        elif new.result == -1:
+            print("O wins!")
+
+        evaluated_len = cache.__len__()
+        print(f"{evaluated_len} positions stored!")
+
 
 main()
-
-evaluated_len = cache.__len__()
-print(f"{evaluated_len} positions stored!")
 
 save_cache(cache, "cache.pkl")
